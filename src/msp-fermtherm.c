d3a45549c0fda0e76a57723c98e61161b12b906a
Lukas Jäger <lukas_jaeger@gmx.net>
2021-05-21 17:50:44 +0200

    Add timer functionality

--- src/msp-fermtherm.c
+++ src/msp-fermtherm.c
@@ -16,45 +16,70 @@
 
 #include <msp430g2553.h>
 
+// IO definitions
 #define BUTTON BIT3
 #define LED2 BIT6
 
+// Timer definitions
+#define TIMER_DELAY (62499) // Equal to 0.5 seconds
+#define ENABLE_TIMER (TASSEL_2 | ID_3 | MC_1 |TACLR)
+
 int main (void)
 {	
-    // Disable WD timer
-    WDTCTL = WDTPW + WDTHOLD;
-    
-		// Make button pin input
-		P1DIR &= ~BUTTON;
-    // Enable pull-up resistor
-		P1REN |= BUTTON;
-	 	// Some inversion magic?
-    P1OUT = BUTTON;
-		// Interrupt at high-to-low transition
-    P1IES |= BUTTON;
-    // Initial interrupt flag clear
-    P1IFG &= ~BUTTON;
-    // Enable interrupt for button
-    P1IE |= BUTTON;
-   
-		// Enable LED2
-    P1DIR |= LED2;
-    
-    // Globally enable interrupts
-    _BIS_SR(GIE);
-    
-		// Loop forever
-		while(1);
+	// Disable WD timer
+	WDTCTL = WDTPW + WDTHOLD;
+
+	// Set clocks
+	BCSCTL1 = CALBC1_1MHZ;
+	DCOCTL = CALDCO_1MHZ;
+	
+	// Make button pin input
+	P1DIR &= ~BUTTON;
+	// Enable pull-up resistor
+	P1REN |= BUTTON;
+	// Some inversion magic?
+	P1OUT = BUTTON;
+	// Interrupt at high-to-low transition
+	P1IES |= BUTTON;
+	// Initial interrupt flag clear
+	P1IFG &= ~BUTTON;
+	// Enable interrupt for button
+	P1IE |= BUTTON;
+
+	// Enable LED2
+	P1DIR |= LED2;
+		
+	// Enable timer interrupt
+	TACCTL0 = CCIE;
+
+	// Globally enable interrupts
+	_BIS_SR(GIE);
+
+	// Loop forever
+	while(1);
 }
 
 // ISR for port 1 
 static void __attribute__((__interrupt__(PORT1_VECTOR))) p1_isr(void) {
-    // Check if button push caused it
-    if (P1IFG & BUTTON) {
-    	// Clear flag
-			P1IFG = P1IFG & ~BUTTON;
-            
-      // Activate LED
-      P1OUT |= LED2;
-    }
-} 
+	// Check if button push caused it
+	if (P1IFG & BUTTON) {
+		// Clear flag
+		P1IFG = P1IFG & ~BUTTON;
+
+		// Activate LED
+		P1OUT |= LED2;
+
+		// Enable and start timer
+		TA0CTL |= ENABLE_TIMER;
+		TACCR0 =  TIMER_DELAY;
+	}
+}
+
+
+// ISR for TimerA0
+static void __attribute__((__interrupt__(TIMER1_A0_VECTOR))) ta0_isr(void) {
+	// Disable LED2
+	P1OUT &= ~LED2;
+	// Disable timer
+	TACCR0 = 0;
+}
